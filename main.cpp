d20634c1ab273782fcb3b696345172f92f56912a
alluvishnu <79898958+alluvishnu@users.noreply.github.com>
2021-03-03 16:50:19 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double m;
+    int n=2;
+    
+    cout<<"Welcome to the Project of Power Function \n";
+    cout<<"Enter  value m\n";
+    cin>>m;
+    cout<<m<<"^"<<n<<"="<<pow(m,2);
+
+    return 0;
+}
